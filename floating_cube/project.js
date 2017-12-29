d8ea01ad01132e5d4bb7eca0ae0c59a6b086894e
marcinrejdych <marcin.rejdych@relayr.io>
2017-12-29 16:27:18 +0100

    added controls and statistics to floating cube project

--- floating_cube/project.js
+++ floating_cube/project.js
@@ -1,3 +1,5 @@
+const controls = initControls();
+const stats = initStats();
 const scene = new THREE.Scene();
 const renderer = createRenderer();
 const camera = setUpCamera(renderer);
@@ -19,15 +21,45 @@ animate();
 
 // infinite animation loop which makes renderer to draw the scene 60 fps
 function animate() {
+    stats.update();
     requestAnimationFrame(animate);
-    offset += 0.05;
-    cube.rotation.x += 0.01;
-    cube.rotation.z += 0.01;
-    cube.rotation.y += 0.03;
+    offset += controls.movingSpeed;
+    cube.material.color.setHex(controls.cubeColor);
+    cube.rotation.x += controls.xRotationSpeed;
+    cube.rotation.z += controls.zRotationSpeed;
+    cube.rotation.y += controls.yRotationSpeed;
     cube.position.y = 0 + ((Math.cos(offset)) / 2);
     renderer.render(scene, camera);
 };
 
+function initControls(){
+    let controls = new function() {
+        this.cubeColor = 0x00ff00;
+        this.movingSpeed = 0.05;
+        this.xRotationSpeed = 0.01;
+        this.yRotationSpeed = 0.03;
+        this.zRotationSpeed = 0.01;
+    }
+    
+    let gui = new dat.GUI();
+    gui.add(controls, 'cubeColor', 0x000000, 0xFFFFFF)
+    gui.add(controls, 'movingSpeed', 0, 0.25)
+    gui.add(controls, 'xRotationSpeed', 0, 0.25)
+    gui.add(controls, 'yRotationSpeed', 0, 0.25)
+    gui.add(controls, 'zRotationSpeed', 0, 0.25)
+
+    return controls;
+}
+
+function initStats(){
+    var stats = new Stats();
+    stats.showPanel(0);
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.left = '0px';
+    stats.domElement.style.top = '0px';
+    document.body.appendChild(stats.dom);
+    return stats;
+}
 
 function createRenderer() {
     let renderer = new THREE.WebGLRenderer();
